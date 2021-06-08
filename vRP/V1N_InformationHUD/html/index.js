d7ab653b95850930eea7eafaf8c6ba2e3dc75dfa
V1NDs <65359820+V1NDs@users.noreply.github.com>
2021-06-08 20:37:59 +0200

    Update index.js

--- vRP/V1N_InformationHUD/html/index.js
+++ vRP/V1N_InformationHUD/html/index.js
@@ -42,7 +42,19 @@ $(document).ready(function() {
                 $('.bars-container > .wallet-bar > h3').text(`${numberTranslate(data.wallet)} DKK`);
                 $('.bars-container > .bank-bar > h3').text(`${numberTranslate(data.bank)} DKK`);
                 $('.bars-container > .dirtymoney-bar > h3').text(`${numberTranslate(data.dirty_money)} DKK`);
-                $('.bars-container > .identity-bar > h3').html(`<span style=' font-weight: bold; '>Job: </span>${data.job}<br><span style=' font-weight: bold; '>ID: </span>${data.id}`);
+
+                if (data.job && data.job !== '') {
+                    $('.bars-container > .identity-bar > h3').html(`<span style=' font-weight: bold; '>Job: </span>${data.job}<br><span style=' font-weight: bold; '>ID: </span>${data.id}`)
+                    return
+                }
+
+                $('.bars-container > .identity-bar > h3').html(`<span style=' font-weight: bold; '>Job: </span>Intet<br><span style=' font-weight: bold; '>ID: </span>${data.id}`)
+                return
+            }
+
+            if (data.type === 'close') {
+                display(false)
+                return
             }
         })
     })
